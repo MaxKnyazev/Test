902dbe889dd9d82c5a4589ca4f5cc6aefabee507
MaxKnyazev <max.knyazev77@gmail.com>
2020-04-21 22:36:01 +0300

    2020-04-21 binarySearch

---
+++ binarySearch/index.js
@@ -0,0 +1,9 @@
+console.log('Started...');
+
+function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+console.log(randomInteger(1, 99));
